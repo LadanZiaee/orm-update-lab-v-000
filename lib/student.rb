54c1ff0fd5dd9f611a78277ef4111b821dc35f1c
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-20 19:52:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,46 @@
 require_relative "../config/environment.rb"
 
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
-
+  attr_accessor :name, :grade
+  attr_reader :id 
+  
+  def initialize(id = nil, name, grade)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY, name, TEXT, grade INTEGER);
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.album)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    end
+  end
+  
+  def self.create(name:, grade:)
+    students = Student.new(name, grade)
+    students.save
+    students
+  end
+  
+  def self.new_from_db(row)
 end
